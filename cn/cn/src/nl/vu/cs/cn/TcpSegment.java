90ba99fca56284b386daa1567d92a86443c42878
Marcin Jedynak <m.jedyny@gmail.com>
2014-05-20 15:53:13 +0200

    Simplified TcpSegment class

--- cn/cn/src/nl/vu/cs/cn/TcpSegment.java
+++ cn/cn/src/nl/vu/cs/cn/TcpSegment.java
@@ -2,66 +2,7 @@ package nl.vu.cs.cn;
 
 import java.nio.ByteBuffer;
 
-public class TcpSegment {
-
-	public class TcpSegmentBuilder {
-
-		public TcpSegmentBuilder from(short from) {
-			segment.from = from;
-			return this;
-		}
-
-		public TcpSegmentBuilder to(short to) {
-			segment.to = to;
-			return this;
-		}
-
-		public TcpSegmentBuilder withSeq(int seq) {
-			segment.seq = seq;
-			return this;
-		}
-
-		public TcpSegmentBuilder withAck(int ack) {
-			segment.ack = ack;
-			segment.flags |= ACK_FLAG;
-			return this;
-		}
-
-		public TcpSegmentBuilder setSyn() {
-			segment.flags |= SYN_FLAG;
-			return this;
-		}
-
-		public TcpSegmentBuilder setFin() {
-			segment.flags |= FIN_FLAG;
-			return this;
-		}
-
-		public TcpSegmentBuilder withData(byte[] data) {
-			segment.data = new byte[data.length];
-			System.arraycopy(data, 0, segment.data, 0, data.length);
-			return this;
-		}
-
-		public TcpSegment build() {
-			return segment;
-		}
-
-		private TcpSegmentBuilder() {
-			this.segment = new TcpSegment();
-			segment.flags |= PUSH_FLAG;
-		}
-
-		private TcpSegment segment;
-	}
-
-	public TcpSegmentBuilder builder() {
-		return new TcpSegmentBuilder();
-	}
-
-	public void setChecksum(short checksum) {
-		this.checksum = checksum;
-	}
+/* package */ final class TcpSegment {
 
 	// since data field in IP.Packet is reused and may contain additional
 	// garbage, "real" data length argument is provided here
@@ -104,71 +45,26 @@ public class TcpSegment {
 	}
 	// @formatter:on
 
-	
-	
-	private TcpSegment() {
-	}
+	/* package */ short from;
 
-	public short getFrom() {
-		return from;
-	}
+	/* package */ short to;
 
-	public short getTo() {
-		return to;
-	}
+	/* package */ int seq;
 
-	public int getSeq() {
-		return seq;
-	}
+	/* package */ int ack;
 
-	public int getAck() {
-		return ack;
-	}
+	/* package */ static short ACK_FLAG = 16;
 
-	public boolean isAckSet() {
-		return (flags & ACK_FLAG) != 0;
-	}
-	
-	public boolean isPushSet() {
-		return (flags & PUSH_FLAG) != 0;
-	}
-	
-	public boolean isSynSet() {
-		return (flags & SYN_FLAG) != 0;
-	}
-	
-	public boolean isFinSet() {
-		return (flags & FIN_FLAG) != 0;
-	}		
+	/* package */ static short PUSH_FLAG = 8;
 
-	public short getChecksum() {
-		return checksum;
-	}
+	/* package */ static short SYN_FLAG = 2;
 
-	public byte[] getData() {
-		return data;
-	}
+	/* package */ static short FIN_FLAG = 1;
 
-	private short from;
+	/* package */ short flags;
 
-	private short to;
+	/* package */ short checksum;
 
-	private int seq;
-
-	private int ack;
-
-	private static short ACK_FLAG = 16;
-
-	private static short PUSH_FLAG = 8;
-
-	private static short SYN_FLAG = 2;
-
-	private static short FIN_FLAG = 1;
-
-	private short flags;
-
-	private short checksum;
-
-	private byte[] data;
+	/* package */ byte[] data;
 
 }
