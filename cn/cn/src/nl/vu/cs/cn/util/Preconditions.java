1811916523637c3788d827f220b673bc176d3f2b
Marcin Jedynak <m.jedyny@gmail.com>
2014-05-20 15:50:00 +0200

    ADD: Simple guava-based preconditions class

---
+++ cn/cn/src/nl/vu/cs/cn/util/Preconditions.java
@@ -0,0 +1,21 @@
+package nl.vu.cs.cn.util;
+
+public final class Preconditions {
+	
+	public static <T> T checkNotNull(T reference) {
+		if (reference == null) {
+			throw new NullPointerException();
+		}
+		return reference;
+	}
+	
+	public static void checkState(boolean condition) {
+		if (!condition) {
+			throw new IllegalStateException();
+		}
+	}
+	
+	private Preconditions() {
+		/* prevent creation */
+	}
+}
