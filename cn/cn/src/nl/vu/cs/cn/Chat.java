2fcfcdbf4ba442456d34cf83be4237607a6f64f2
Marcin Jedynak <m.jedyny@gmail.com>
2014-06-30 17:06:42 +0200

    Fix: Chat messages are now shown correctly

--- cn/cn/src/nl/vu/cs/cn/Chat.java
+++ cn/cn/src/nl/vu/cs/cn/Chat.java
@@ -1,183 +1,195 @@
 package nl.vu.cs.cn;
 
-
 import java.io.IOException;
 
 import nl.vu.cs.cn.IP.IpAddress;
 import nl.vu.cs.cn.TCP.Socket;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Chat extends Activity {
 	/** Called when the activity is first created. */
-	 
-	TextView serverTextView, clientTextView; // Member variable for text view in the layout
-	EditText serverMessage, clientMessage; 
-	Button serverButtonSend, clientButtonSend;
-	public static int CLIENT_IP = 1;
-	public static int SERVER_IP = 10;
-	public static int SERVER_PORT = 4444;
-	Socket client, server;
+
+	TextView upperTextView, lowerTextView; // Member variable for text view in
+											// the layout
+	EditText upperInputField, lowerInputField;
+	public static int UPPER_IP = 1;
+	public static int LOWER_IP = 10;
+	public static int LOWER_PORT = 4444;
+	Socket lower, upper;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		
+
 		// Initialize member TextView so we can manipulate it later
-	    serverTextView = (TextView) findViewById(R.id.server_text_message);
-	    clientTextView = (TextView) findViewById(R.id.client_text_message);
+		upperTextView = (TextView) findViewById(R.id.upper_chat_window);
+		lowerTextView = (TextView) findViewById(R.id.lower_chat_window);
 
-		serverMessage = (EditText) findViewById(R.id.server_message);
-		clientMessage = (EditText) findViewById(R.id.client_message);
+		upperTextView.setMovementMethod(new ScrollingMovementMethod());
+		lowerTextView.setMovementMethod(new ScrollingMovementMethod());
 		
-		serverButtonSend = (Button)findViewById(R.id.server_button);
-		clientButtonSend = (Button)findViewById(R.id.client_button);	
-		
-		
-		serverButtonSend.setOnClickListener(new OnClickListener(){
-		
-			public void onClick(View arg0) {
-				
-				String serverMsg = serverMessage.getText().toString();
-				final byte[] serverWriteBuf = serverMsg.getBytes();
-				final byte[] clientReadBuf = new byte[serverWriteBuf.length];
-				
-				Runnable serverTask = new Runnable() {
-					@Override public void run() {
-						server.write(serverWriteBuf, 0, serverWriteBuf.length);
-						
+		upperInputField = (EditText) findViewById(R.id.upper_input_field);
+		lowerInputField = (EditText) findViewById(R.id.lower_input_field);
+
+		upperInputField.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN)
+						&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+					String upperMsg = upperInputField.getText().toString();
+					final byte[] upperWriteBuf = upperMsg.getBytes();
+					final byte[] lowerReadBuf = new byte[upperWriteBuf.length];
+
+					Runnable upperTask = new Runnable() {
+						@Override
+						public void run() {
+							upper.write(upperWriteBuf, 0, upperWriteBuf.length);
+
+						}
+					};
+
+					Runnable lowerTask = new Runnable() {
+						@Override
+						public void run() {
+							lower.read(lowerReadBuf, 0, lowerReadBuf.length);
+						}
+					};
+
+					Thread upperThread = new Thread(upperTask);
+					upperThread.start();
+					lowerTask.run();
+					try {
+						upperThread.join();
+					} catch (InterruptedException e) {
+						Log.e("Interrupted", "Error", e);
 					}
-				};
-				
-				Runnable clientTask = new Runnable() {
-					@Override public void run() {
-						client.read(clientReadBuf, 0, clientReadBuf.length);
-					}
-				};
-				
-				Thread serverThread = new Thread(serverTask);
-				serverThread.start();
-				clientTask.run();
-				try {
-					serverThread.join();
-				} catch (InterruptedException e) {
-					Log.e("Interrupted", "Error",e );
+
+					String receivedMsg = new String(lowerReadBuf);
+					lowerTextView.append(receivedMsg + "\n");
+					upperInputField.setText("");
+					return true;
 				}
-				
-				String receivedMsg = new String(clientReadBuf);
-				clientTextView.append(receivedMsg+ "\n");
-				serverMessage.setText("");
-
-		    }
+				return false;
+			}
 		});
-	
-		clientButtonSend.setOnClickListener(new OnClickListener(){
-			
-			public void onClick(View arg0) {
-				
-				String clientMsg = clientMessage.getText().toString();				
-				final byte[] clientWriteBuf = clientMsg.getBytes();
-				final byte[] serverReadBuf = new byte[clientWriteBuf.length];
-				
-				Runnable serverTask = new Runnable() {
-					@Override public void run() {
-						server.read(serverReadBuf, 0, serverReadBuf.length);
+
+		lowerInputField.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN)
+						&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+					String lowerMsg = lowerInputField.getText().toString();
+					final byte[] lowerWriteBuf = lowerMsg.getBytes();
+					final byte[] upperReadBuf = new byte[lowerWriteBuf.length];
+
+					Runnable upperTask = new Runnable() {
+						@Override
+						public void run() {
+							upper.read(upperReadBuf, 0, upperReadBuf.length);
+						}
+					};
+
+					Runnable lowerTask = new Runnable() {
+						@Override
+						public void run() {
+							lower.write(lowerWriteBuf, 0, lowerWriteBuf.length);
+						}
+					};
+
+					Thread upperThread = new Thread(upperTask);
+					upperThread.start();
+					lowerTask.run();
+					try {
+						upperThread.join();
+					} catch (InterruptedException e) {
+						Log.e("Interrupted", "Error", e);
 					}
-				};
-				
-				Runnable clientTask = new Runnable() {
-					@Override public void run() {
-						client.write(clientWriteBuf, 0, clientWriteBuf.length);	
-					}
-				};
-				
-				Thread serverThread = new Thread(serverTask);
-				serverThread.start();
-				clientTask.run();
-				try {
-					serverThread.join();
-				} catch (InterruptedException e) {
-					Log.e("Interrupted", "Error",e );
+
+					String receivedMsg = new String(upperReadBuf);
+					upperTextView.append(receivedMsg + "\n");
+					lowerInputField.setText("");
+					return true;
 				}
-				
-				String receivedMsg = new String(serverReadBuf);
-				serverTextView.append(receivedMsg + "\n");
-				clientMessage.setText("");
-				
-		    }
-		});			
+				return false;
+			}
+		});
 	}
-			 
-	
+
 	public void onPause() {
-	    super.onPause();  // Always call the superclass method first
-	    
-	    Runnable serverTask = new Runnable() {
-			@Override public void run() {
-				server.close();
+		super.onPause(); // Always call the superclass method first
+
+		Runnable upperTask = new Runnable() {
+			@Override
+			public void run() {
+				upper.close();
 			}
 		};
-		
-		Runnable clientTask = new Runnable() {
-			@Override public void run() {
-				client.close();	
+
+		Runnable lowerTask = new Runnable() {
+			@Override
+			public void run() {
+				lower.close();
 			}
 		};
-		
-		Thread serverThread = new Thread(serverTask);
-		serverThread.start();
-		clientTask.run();
+
+		Thread upperThread = new Thread(upperTask);
+		upperThread.start();
+		lowerTask.run();
 		try {
-			serverThread.join();
+			upperThread.join();
 		} catch (InterruptedException e) {
-			Log.e("Interrupted", "Error",e );
+			Log.e("Interrupted", "Error", e);
 		}
-		  
+
 	}
-	
+
 	@Override
 	public void onResume() {
-	    super.onResume();  // Always call the superclass method first
-	    
-	    try {
-			
-			client = new TCP(CLIENT_IP).socket();
-			server = new TCP(SERVER_IP).socket(SERVER_PORT);
-			
-			Runnable clientTask = new Runnable() {
-				@Override public void run() {
-					IpAddress serverAddr = IpAddress.getAddress("192.168.0." + SERVER_IP);
-					client.connect(serverAddr, SERVER_PORT);
+		super.onResume(); // Always call the superclass method first
+
+		try {
+
+			lower = new TCP(UPPER_IP).socket();
+			upper = new TCP(LOWER_IP).socket(LOWER_PORT);
+
+			Runnable lowerTask = new Runnable() {
+				@Override
+				public void run() {
+					IpAddress upperAddr = IpAddress.getAddress("192.168.0."
+							+ LOWER_IP);
+					lower.connect(upperAddr, LOWER_PORT);
 				}
 			};
-			
-			Runnable serverTask = new Runnable() {
-				@Override public void run() {
-					server.accept();
+
+			Runnable upperTask = new Runnable() {
+				@Override
+				public void run() {
+					upper.accept();
 				}
 			};
-			
-			Thread serverThread = new Thread(serverTask);
-			serverThread.start();
-			clientTask.run();
-			serverThread.join();
+
+			Thread upperThread = new Thread(upperTask);
+			upperThread.start();
+			lowerTask.run();
+			upperThread.join();
 		} catch (IOException ioe) {
-			Log.e("TCP", "Error",ioe );
-		}catch (InterruptedException e) {
-			Log.e("TCP", "Error",e);
+			Log.e("TCP", "Error", ioe);
+		} catch (InterruptedException e) {
+			Log.e("TCP", "Error", e);
 		}
 	}
-	
-	public void onDestroy(){
-		super.onDestroy(); 
+
+	public void onDestroy() {
+		super.onDestroy();
 
 	}
 }
