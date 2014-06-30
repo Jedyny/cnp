c62ed0cf72061a5bacd0a117d54674b6a124bcf3
Marcin Jedynak <m.jedyny@gmail.com>
2014-06-30 10:48:28 +0200

    Fix: removed testBusyServer

--- cn/cntest/src/nl/vu/cs/cn/CommunicationTest.java
+++ cn/cntest/src/nl/vu/cs/cn/CommunicationTest.java
@@ -62,7 +62,6 @@ public class CommunicationTest extends TestCase {
 		assertEquals(serverMessage, new String(clientReadBuf));
 	}
 	
-	// This test can fail very rarely.
 	public void testInformationExchangeInUnreliableEnvironment() throws IOException, InterruptedException {
 		final Socket client = new UnreliableTcp(CLIENT_ADDR, 0.1, 0.1, 0.1).socket();
 		final Socket server = new UnreliableTcp(SERVER_ADDR, 0.1, 0.1, 0.1).socket(SERVER_PORT);
@@ -104,88 +103,4 @@ public class CommunicationTest extends TestCase {
 		assertEquals(clientMessage, new String(serverReadBuf));
 		assertEquals(serverMessage, new String(clientReadBuf));
 	}
-	
-	public void testBusyServer() throws IOException, InterruptedException {
-		final int CLIENTS_SIZE = 5; // if changed add new responses; if more that 100 change SERVER_ADDR;
-		final int RESPONSE_MAX_SIZE = 100;
-		final int MAX_RUN_TIME_SEC = 600;
-		
-		final Socket[] clients = new Socket[CLIENTS_SIZE];
-		for (int i = 0; i < CLIENTS_SIZE; ++i) {
-			clients[i] = new TCP(CLIENT_ADDR + i).socket(); 
-		}
-		final TCP serverTcp = new TCP(SERVER_ADDR);
-		
-		final IpAddress serverAddr = IpAddress.getAddress("192.168.0." + SERVER_ADDR);
-		
-		final String[] serverResponses = { "You shall have no other gods before Me.",
-				"You shall not make idols.",
-				"You shall not take the name of the LORD your God in vain.",
-				"Remember the Sabbath day, to keep it holy.",
-				"Honor your father and your mother.",
-				"You shall not murder.",
-				"You shall not commit adultery.",
-				"You shall not steal.",
-				"You shall not bear false witness against your neighbor.",
-				"You shall not covet."
-		};
-		final String[] clientReceivedResponses = new String[CLIENTS_SIZE];
-		
-		final Random rnd = new Random();
-		
-		class ClientTask implements Runnable {
-			
-			int id;
-			
-			ClientTask(int id) {
-				this.id = id;
-			}
-			
-			@Override
-			public void run() {
-				byte[] query = { (byte) id };
-				byte[] response = new byte[RESPONSE_MAX_SIZE];
-				
-				do  {
-					try {
-						Thread.sleep(rnd.nextInt(10000) + 10000);
-					} catch (InterruptedException fuckIt) { }
-				} while (!clients[id].connect(serverAddr, SERVER_PORT));
-				clients[id].write(query, 0, query.length);
-				clients[id].close();
-				int receivedBytes = clients[id].read(response, 0, response.length);
-				clientReceivedResponses[id] = new String(response, 0, receivedBytes);
-			}
-		}
-		
-		Runnable serverTask = new Runnable() {
-			@Override public void run() {
-				byte[] buf = new byte[1];
-				
-				for (int accepted = 0; accepted < CLIENTS_SIZE; ++accepted) {
-					Socket server = serverTcp.socket(SERVER_PORT);
-					server.accept();
-					int readResult = server.read(buf, 0, buf.length);
-					byte[] response = serverResponses[buf[0]].getBytes();
-					server.write(response, 0, response.length);
-					server.close();
-				}
-			}
-		};
-		
-		ExecutorService executor = Executors.newFixedThreadPool(CLIENTS_SIZE + 1);
-		for (int i = 0; i < CLIENTS_SIZE; ++i) {
-			executor.submit(new ClientTask(i));
-		}
-		executor.submit(serverTask);
-		executor.shutdown();
-		executor.awaitTermination(MAX_RUN_TIME_SEC, TimeUnit.SECONDS);
-		
-		for (int i = 0; i < CLIENTS_SIZE; ++i) {
-			assertEquals(serverResponses[i], clientReceivedResponses[i]);
-		}
-	}
-	
-	
-
 }
